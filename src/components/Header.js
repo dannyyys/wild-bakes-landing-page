eba1f51fe15456ffa5dcaa7d6ae95b1dcbf27a08
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-23 11:53:57 +0800

    style: :lipstick: Change Header to only contain OrderNowButton

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,11 @@
 import { Box, Flex, Button, Spacer, Image } from "@chakra-ui/react";
+import { FaWhatsapp } from "react-icons/fa";
 import { OrderNowButton } from "./button/OrderNowButton";
 
 export const Header = () => {
   return (
-    <Box backgroundColor="#132C36" align="center" boxShadow="2xl">
-      <Flex
-        direction={["column", "row"]}
-        align="baseline"
-        paddingBottom="15px"
-        width="80vw"
-        align="center"
-      >
-        <OrderNowButton />
-        <Spacer />
-        <Button backgroundColor="#132C36" size="md" color="white" fontSize="25">
-          ABOUT
-        </Button>
-        <Button backgroundColor="#132C36" size="md" color="white" fontSize="25">
-          TESTIMONIALS
-        </Button>
-        <Button backgroundColor="#132C36" size="md" color="white" fontSize="25">
-          CONTACT
-        </Button>
-        <Image src="logo.png" boxSize="100" />
-      </Flex>
+    <Box backgroundColor="#132C36" boxShadow="2xl" padding="2" align="center">
+      <OrderNowButton />
     </Box>
   );
 };
