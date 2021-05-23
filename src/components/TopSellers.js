84cedc2222ba9a6d022ba49208196054430c49b5
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-23 12:51:37 +0800

    fix: :lipstick: Fix TopSellers style

--- src/components/TopSellers.js
+++ src/components/TopSellers.js
@@ -6,35 +6,37 @@ export const TopSellers = () => {
       <Heading as="h3" size="lg" textColor="#F8F5ED">
         Our Top Sellers
       </Heading>
+
       <Stack
         direction={["column", "row"]}
         marginTop="10"
         align="center"
         width="70vw"
-        spacing="5"
+        spacing="4"
+        marginBottom={[0, -3]}
       >
-        <Box boxSize={["3xs", "xs"]}>
+        <Box boxSize={["3xs", "2xs"]}>
           <Image
             src="ᴄʜᴏᴄᴏʟᴀᴛᴇ ᴄʀᴇᴀᴍ ᴄʜᴏᴜx ᴀᴜ ᴄʀᴀǫᴜᴇʟɪɴ.jpg"
             borderRadius="2xl"
             boxShadow="2xl"
           />
         </Box>
-        <Box boxSize={["3xs", "xs"]}>
+        <Box boxSize={["3xs", "2xs"]}>
           <Image
             src="ʜᴀᴢᴇʟɴᴜᴛ ᴄʜᴏᴄᴏʟᴀᴛᴇ ɢᴀɴᴀᴄʜᴇ ᴛᴀʀᴛᴇ.jpg"
             borderRadius="2xl"
             boxShadow="2xl"
           />
         </Box>
-        <Box boxSize={["3xs", "xs"]}>
+        <Box boxSize={["3xs", "2xs"]}>
           <Image
             src="Choux Au Craquelin.jpg"
             borderRadius="2xl"
             boxShadow="2xl"
           />
         </Box>
-        <Box boxSize={["3xs", "xs"]}>
+        <Box boxSize={["3xs", "2xs"]}>
           <Image
             src="ʜᴀᴢᴇʟɴᴜᴛ ᴄʜᴏᴄᴏʟᴀᴛᴇ ɢᴀɴᴀᴄʜᴇ ᴛᴀʀᴛᴇ.jpg"
             borderRadius="2xl"
