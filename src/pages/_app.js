1af2739cbc10560a0169140dbe8d5f8f0e21259e
dannyyys <dannyyys@users.noreply.github.com>
2021-05-24 23:42:18 +0800

    style: :lipstick: Add meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,27 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
+import Head from "next/head";
 import "@fontsource/cinzel";
 
 import theme from "../theme";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
+    <>
+      <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: true,
+          }}
+        >
+          <Head>
+            <title>WILD BAKES</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </>
   );
 }
 
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,29 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import { ColorModeScript } from '@chakra-ui/react'
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { ColorModeScript } from "@chakra-ui/react";
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta name="title" content="WILD BAKES" />
+          <meta name="description" content="Bakes for the wild souls" />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://metatags.io/" />
+          <meta property="og:title" content="WILD BAKES" />
+          <meta property="og:description" content="Bakes for the wild souls" />
+          <meta property="og:image" content="seo_image.png" />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://metatags.io/" />
+          <meta property="twitter:title" content="WILD BAKES" />
+          <meta
+            property="twitter:description"
+            content="Bakes for the wild souls"
+          />
+          <meta property="twitter:image" content="seo_image.png" />
+        </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
@@ -13,6 +31,6 @@ export default class Document extends NextDocument {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
