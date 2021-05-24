12c37b9c4b229e5ee9731e5170cbd38b490ca46e
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-24 08:20:29 +0800

    style: :lipstick: Add brand colors to theme.js

--- src/theme.js
+++ src/theme.js
@@ -7,6 +7,12 @@ const fonts = {
 
 const theme = extendTheme({
   fonts,
+  colors: {
+    primary: '#E9E3D9',
+    secondary: '#844b55',
+    secondaryBackground: '#905d66',
+    text: '#253F4E',
+  }
 });
 
 export default theme;
