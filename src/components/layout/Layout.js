e6bd5c548ce889b21756becf3d32282985449e5c
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-24 08:20:47 +0800

    style: :lipstick: Change background color

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 
 export const Layout = ({ children }) => {
   return (
-    <Box width="full" height="full" backgroundColor="#253F4E">
+    <Box width="full" height="full" backgroundColor="primary">
       {children}
     </Box>
   );
