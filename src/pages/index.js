791db1e421b2f8e4b77f32203e43922322266d9e
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-25 22:27:53 +0800

    refactor: :fire: Remove unused code from index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 
 import { Layout } from "../components/layout/Layout";
 import { Footer } from "../components/layout/Footer";
 import { Intro } from "../components/introduction/Intro";
 import { Testimonial } from "../components/testimonial/Testimonial";
-import { TopSellers } from "../components/topsellers/TopSellers";
 import { Insta } from "../components/Insta";
 
 const MotionBox = motion(Box);
